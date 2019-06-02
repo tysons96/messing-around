1aac0ca9af56a539e406aade0bf5242c2da08248
tysons96 <31372608+tysons96@users.noreply.github.com>
2019-06-02 12:56:17 -0700

    Add files via upload

---
+++ my_tax_rate.py
@@ -0,0 +1,15 @@
+
+my_income = 100.0
+
+tax_rate = 0.15
+
+my_taxes = (my_income * tax_rate)
+
+
+print('your income is : ')
+print(my_income)
+print("Your Tax Rate is ")
+print(my_taxes) 
+print('your income after taxes is')
+
+print(my_income - my_taxes)
